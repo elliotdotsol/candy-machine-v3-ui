7ea3b110b6e71d8841ee39bdfea155db0ada62a3
crazyrick4 <hello@zaab.studio>
2022-03-28 15:05:20 +0200

    Icon update

--- src/Home.tsx
+++ src/Home.tsx
@@ -23,6 +23,14 @@ const cluster = process.env.REACT_APP_SOLANA_NETWORK!.toString();
 const decimals = process.env.REACT_APP_SPL_TOKEN_TO_MINT_DECIMALS ? +process.env.REACT_APP_SPL_TOKEN_TO_MINT_DECIMALS!.toString() : 9;
 const splTokenName = process.env.REACT_APP_SPL_TOKEN_TO_MINT_NAME ? process.env.REACT_APP_SPL_TOKEN_TO_MINT_NAME.toString() : "TOKEN";
 
+const Twitter = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+      <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+    </svg>
+  )
+}
+
 const WalletContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -42,17 +50,26 @@ const IconContainer = styled.div`
 `;
 
 const InfoContainer = styled.div`
-  display: block;
+  display: inline-flex;
 `;
 
 const SocialIcon = styled.img`
   font-size: 18px;
+  color: #fff;
   `;
 
 const TotalItems = styled.p`
   font-size: 16px;
   font-family: 'Poppins';
   font-weight: 400;
+  margin: 0;
+`;
+
+const Price = styled.p`
+  font-weight: 700;
+  font-size: 16px !important;
+  font-family: 'Poppins' !important;
+  margin: 0;
 `;
 
 const WalletAmount = styled.div`
@@ -205,12 +222,6 @@ const RightContainer = styled.div`
   width: 50%;
   max-width: 50%;
   text-align: left;
-`;
-
-const Price = styled.p`
-  font-weight: 700;
-  font-size: 16px !important;
-  font-family: 'Poppins' !important;
 `;
 
 const Image = styled.img`
@@ -548,7 +559,7 @@ const Home = (props: HomeProps) => {
             <IconContainer>
               <a href="#" target="__blank"> <SocialIcon src="website.svg"></SocialIcon> </a>
               <a href="#" target="__blank"> <SocialIcon src="discord.svg"></SocialIcon> </a>
-              <a href="#" target="__blank"> <SocialIcon src="twitter.svg"></SocialIcon> </a>
+              <a href="#" target="__blank"> <Twitter> </a>
             </IconContainer>
           </InfoContainer>
           {wallet && isActive && whitelistEnabled && (whitelistTokenBalance > 0) && isBurnToken &&

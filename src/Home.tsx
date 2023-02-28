1244c92d6c462d336668e30e23578f29021da534
wearekite <hello@kite.studio>
2023-02-28 21:16:55 +0100

    Small cleanup of dummy content

--- src/Home.tsx
+++ src/Home.tsx
@@ -358,7 +358,7 @@ const MintGroupWrap = styled.div`
   gap: 16px;  
   width: 100%;
   max-height: 500px;
-  overflow-y: scroll;
+  overflow-y: auto;
 `
 const MintGroupItem = styled.div`
   display: flex;

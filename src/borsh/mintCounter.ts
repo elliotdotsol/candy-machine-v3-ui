19b4fe21b9df7a56b6b830c9772e9a30dca39d9a
wearekite <hello@kite.studio>
2023-04-08 16:10:10 +0200

    Update mintCounter.ts

--- src/borsh/mintCounter.ts
+++ src/borsh/mintCounter.ts
@@ -1,9 +1,9 @@
-import { MintCounterArgs } from "@metaplex-foundation/mpl-candy-guard";
+import { MintCounter } from "@metaplex-foundation/mpl-candy-guard";
 import { deserialize } from "borsh";
 
-export class MintCounterBorsh implements MintCounterArgs {
+export class MintCounterBorsh implements MintCounter {
   count: number;
-  constructor(args: MintCounterArgs) {
+  constructor(args: MintCounter) {
     Object.assign(this, args);
   }
   static schema = new Map([
